69d29b08311b9fc46b694c883cfe4d828e07ada4
SEO <malacma@gmail.com>
2017-07-18 04:26:08 -0300

    GUIA

--- serverside/GuiaController.php
+++ serverside/GuiaController.php
@@ -8,30 +8,56 @@ DB::$host = 'localhost';
 DB::$port = '3306';
 DB::$error_handler = 'my_error_handler';
 
-class GuiaController extends stdClass{
+class GuiaController extends stdClass {
 
-    public static function getEventosDeHoje($object){
-        //@todo
-        //DB::query("SELECT * FROM Profile"); // misspelled SELECT
-
-        return null;
-    }  
+    public static function getEventosDeHoje() {
+        $today = date("Y-m-d");
+        $query = "select * from viewEventPlaces where dtUntil >= '$today'";
+        $eventos = DB::query($query); // misspelled SELECT
+        //Return Object
+        $stdGuia = new stdClass();
+        $stdGuia->e = [];
+        foreach ($eventos as $row) {
+            $std = new stdClass();
+            $std->nrPhone = $row['nrPhone'];
+            $std->deLogo = $row['deLogo'];
+            $std->deAddress = $row['deAddress'];
+            $std->deEvent = $row['deEvent'];
+            $std->deDetail = $row['deDetail'];
+            $std->dtFrom = $row['dtFrom'];
+            $std->dtUntil = $row['dtUntil'];
+            $std->idType = $row['idType'];
+            $std->nrCep = $row['nrCep'];
+            $std->nrLat = $row['nrLat'];
+            $std->nrLng = $row['nrLng'];
+            //Adiciona
+            $stdGuia->e[] = $std;
+        }
+        //get types descriptions
+        $query = "SELECT * FROM Type;";
+        //RUn qyery
+        $types = DB::query($query); // misspelled SELECT
+        //Prepare another return
+        $stdGuia->t = [];
+        foreach ($types as $row) {
+            $std = new stdClass();
+            $std->idType = $row['idType'];
+            $std->deType = $row['deType'];
+            $stdGuia->t[] = $std;
+        }
+        //Close Connection
+        DB::disconnect();
+        return $stdGuia;
+    }
 
 }
 
-
-
-
-
-
-
-
-/******
+/* * ****
  *    Database Handlers
  */
 
 function my_error_handler($params) {
-  echo "Error: " . $params['error'] . "<br>\n";
-  echo "Query: " . $params['query'] . "<br>\n";
-  die; // don't want to keep going if a query broke
+    echo "Error: " . $params['error'] . "<br>\n";
+    echo "Query: " . $params['query'] . "<br>\n";
+    die; // don't want to keep going if a query broke
 }
--- serverside/index.php
+++ serverside/index.php
@@ -1,4 +1,5 @@
 <?php
+
 //header("Access-Control-Allow-Origin: *");
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -11,44 +12,37 @@ require 'GuiaController.php';
 $app = new \Slim\App;
 //$app->view(new \JsonApiView());
 //$app->add(new \JsonApiMiddleware());
-
-
-
 //Define Routes
 //Busca Eventos de Hoje
-$app->get('/busca/{lat}/{lon}/{types}', function (Request $request, Response $response) use ($app) {
-    //Content Type JSONP Cross Domain JSON
-    $newResponse = $response->withHeader('Content-type', 'application/json');
-    //Get Parameters
-    $data = new stdClass();
-	$data->lat = $request->getAttribute('lat');
-    $data->lon = $request->getAttribute('lon');
-    $data->types = $request->getAttribute('types');
+$app->get('/busca/', function (Request $request, Response $response) use ($app) {
+    //Content Type JSON Cross Domain JSON
+    $newResponse = $response->withHeader('Content-type', 'application/json')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     //Return Eventos for today
-    $data->eventos = GuiaController::getEventosDeHoje($data);
+    $data = GuiaController::getEventosDeHoje();
     //Response Busca Hoje
-    //echo "buscaHoje({})";
-   
-
     return $newResponse->withJson($data, 201);
 });
 
 //Busca eventos de uma determinada data
-$app->get('/busca/{lat}/{lon}/{types}/{from}/{to}', function (Request $request, Response $response) use ($app){
-    //Content Type JSONP Cross Domain JSON
-    //$app->contentType('application/javascript');
-    //Get Parameters
+$app->get('/busca/{types}/{from}/{to}', function (Request $request, Response $response) use ($app) {
+    $newResponse = $response->withHeader('Content-type', 'application/json')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    //StdObject for return
     $data = new stdClass();
-	$data->lat = $request->getAttribute('lat');
-    $data->lon = $request->getAttribute('lon');
     $data->types = $request->getAttribute('types');
     $data->from = $request->getAttribute('from');
     $data->to = $request->getAttribute('to');
     //Return Eventos by date interval
     $eventos = GuiaController::getEventosDeHoje($data);
-    //Response Busca Data    
-    echo "buscaData({})";
+    //Response Busca Data
+    return $newResponse->withJson($data, 201);
 });
 $app->run();
+
 
 
