de996d3cba03de013ea7f07e5900e6ba8381ca61
CEO <malacma@gmail.com>
2017-12-20 03:47:52 -0200

    Painel de administração do anuncio cadastro e edição import do facebook

--- guia_backend/plugin_appguia/views/EventControl.php
+++ guia_backend/plugin_appguia/views/EventControl.php
@@ -851,5 +851,34 @@ function GetImageFromUrl($link) {
     $savefile = fopen($basedir, 'w');
     fwrite($savefile, $result);
     fclose($savefile);
+    createAttach($basedir);
     return $basedir;
+}
+
+function createAttach($filename) {
+    // $filename should be the path to a file in the upload directory.
+// Check the type of file. We'll use this as the 'post_mime_type'.
+    $filetype = wp_check_filetype(basename($filename), null);
+
+// Get the path to the upload directory.
+    $wp_upload_dir = wp_upload_dir();
+
+// Prepare an array of post data for the attachment.
+    $attachment = array(
+        'guid' => $wp_upload_dir['url'] . '/' . basename($filename),
+        'post_mime_type' => $filetype['type'],
+        'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
+        'post_content' => '',
+        'post_status' => 'inherit'
+    );
+
+// Insert the attachment.
+    $attach_id = wp_insert_attachment($attachment, $filename);
+
+// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+    require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+// Generate the metadata for the attachment, and update the database record.
+    $attach_data = wp_generate_attachment_metadata($attach_id, $filename);
+    wp_update_attachment_metadata($attach_id, $attach_data);
 }
